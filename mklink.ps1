bec2e850ecf7514b81db7c56f92dcf52cf4ac56f
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2025-08-24 11:02:32 +0600

    Auto-commit

--- mklink.ps1
+++ mklink.ps1
@@ -1,2 +1,3 @@
 New-Item -ItemType SymbolicLink -Path "C:\Users\nahid\ms\ms1\scripts\flask\5013_movie_tracker\data.json" -Target "C:\Users\nahid\ms\db\5013_movie_tracker\data.json" -Force
+New-Item -ItemType SymbolicLink -Path "C:\Users\nahid\ms\ms1\scripts\flask\5011_tv_show\data.json" -Target "C:\Users\nahid\ms\db\5011_tv_show\data.json" -Force
 Pause
