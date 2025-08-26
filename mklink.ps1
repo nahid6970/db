1f43420e9caaab8dded50b6272894d3539e42cf2
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2025-08-26 22:52:07 +0600

    Auto-commit

--- mklink.ps1
+++ mklink.ps1
@@ -1,5 +1,5 @@
 New-Item -ItemType SymbolicLink -Path "C:\Users\nahid\ms\ms1\scripts\flask\5013_movie_tracker\data.json" -Target "C:\Users\nahid\ms\db\5013_movie_tracker\data.json" -Force
 New-Item -ItemType SymbolicLink -Path "C:\Users\nahid\ms\ms1\scripts\flask\5011_tv_show\data.json" -Target "C:\Users\nahid\ms\db\5011_tv_show\data.json" -Force
-New-Item -ItemType SymbolicLink -Path "C:\Users\nahid\ms\ms1\myhome\data.json" -Target "C:\Users\nahid\ms\db\5000_myhome\data.json" -Force
+New-Item -ItemType SymbolicLink -Path "C:\Users\nahid\ms\ms1\scripts\flask\5000_myhome\data.json" -Target "C:\Users\nahid\ms\db\5000_myhome\data.json" -Force
 New-Item -ItemType SymbolicLink -Path "C:\Users\nahid\ms\ms1\scripts\flask\5003_Browse_PC_Files\bookmarks.json" -Target "C:\Users\nahid\ms\db\5003_Browse_PC_Files\bookmarks.json" -Force
 Pause
