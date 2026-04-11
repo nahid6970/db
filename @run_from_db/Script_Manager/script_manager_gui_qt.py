38f5f0eb39ec7c59dbd6e999cae93a6b0da2069d
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2026-04-11 10:03:41 +0600

    updated svg

--- @run_from_db/Script_Manager/script_manager_gui_qt.py
+++ @run_from_db/Script_Manager/script_manager_gui_qt.py
@@ -1517,7 +1517,7 @@ SVGS = {
     "DOWNLOAD": '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg>',   
     "TRASH": '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2-0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>',
     "DIFF": '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="16 18 22 12 16 6"></polyline><polyline points="8 6 2 12 8 18"></polyline></svg>',
-    "CLOUD": '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 10h-1.26A8 8 0 1 0 9 20h9a5 5 0 0 0 0-10z"></path></svg>'
+    "SYNC": '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 12a9 9 0 0 1 9-9 9.75 9.75 0 0 1 6.74 2.74L21 8"></path><path d="M21 3v5h-5"></path><path d="M21 12a9 9 0 0 1-9 9 9.75 9.75 0 0 1-6.74-2.74L3 16"></path><path d="M3 21v-5h5"></path></svg>'
 }
 
 class ConvexButton(QPushButton):
@@ -2021,7 +2021,7 @@ class MainWindow(QMainWindow):
         header.addWidget(self.inp_search) # Add search here
         header.addWidget(self.btn_add_script)
         
-        self.btn_cloud = ConvexButton("", color=CP_CYAN, is_outlined=True, svg_data=SVGS["CLOUD"])
+        self.btn_cloud = ConvexButton("", color=CP_CYAN, is_outlined=True, svg_data=SVGS["SYNC"])
         self.btn_cloud.setFixedSize(35, 35)
         self.btn_cloud.setToolTip("Cloud Sync: Backup & Restore")
         self.btn_cloud.clicked.connect(self.open_cloud_sync)
