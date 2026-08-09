dba94413442b4713fef0f7d2a67ea92e894f7862
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2026-08-09 13:47:11 +0600

    reverted vlr

--- vlr/api/matches/all.js
+++
@@ -1,6 +0,0 @@
-import handler from "../matches.js";
-
-export default function allHandler(req, res) {
-  req.url = "/api/matches?action=all";
-  return handler(req, res);
-}
--- vlr/api/matches/view.js
+++
@@ -1,6 +0,0 @@
-import handler from "../matches.js";
-
-export default function viewHandler(req, res) {
-  req.url = "/api/matches?action=view";
-  return handler(req, res);
-}
